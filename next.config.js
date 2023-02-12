e2a1f6bdcac6c63f3acec626122cbedebed46ebd
japolinar <japolinar_1@hotmail.com>
2023-02-12 18:58:14 -0300

    modif-next.config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@
 module.exports = {
   env: {
     //backendURL: 'http://localhost:4000',
-    backendURL: 'http://127.0.0.1:4000',    
-    frontendURL: 'http://localhost:3000'
+    // backendURL: 'http://127.0.0.1:4000',    
+    // frontendURL: 'http://localhost:3000'
+
+    backendURL: 'https://nodesend-servidor-production.up.railway.app',  //<-- aca colocar la URL de "railway"
+    frontendURL: 'https://nodesend-cliente-plum.vercel.app'             //<-- aca colocar la URL de "vercel"
   }
 }
